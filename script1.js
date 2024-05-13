5acb6cd11dd2379f9aa35345bd4f9f2425a7c8f7
JagannathSM <167777809+JagannathSM@users.noreply.github.com>
2024-05-13 20:31:09 +0530

    getElementById.value_METHOD

---
+++ script1.js
@@ -0,0 +1,10 @@
+function getvalues(){
+    var first=document.getElementById("first").value;
+    var middle=document.getElementById("middle").value;
+    var last=document.getElementById("last").value;
+    var email=document.getElementById("email").value;
+    var address=document.getElementById("add").value;
+
+    console.log(first,middle,last,email,address);
+
+}
